a081667f914238fc03fc3ab2be5aa3bcfcb59b79
Michael Lee <lee.5151@osu.edu>
2018-08-06 22:58:24 -0400

    prepare exercise folders and files

---
+++ 1_the_basics/exercise_2.rb
@@ -0,0 +1,18 @@
+def four_digits
+  puts "Give a 4 digit number: "
+  num = gets.to_i
+  if num / 1000 != 0
+    thousands = num / 1000
+    hundreds = num % 1000 / 100
+    tens = num % 1000 % 100 / 10
+    ones = num % 1000 % 100 % 10 / 1
+    puts "thousands place: #{thousands}"
+    puts "hundreds place: #{hundreds}"
+    puts "tens place: #{tens}"
+    puts "ones place: #{ones}"
+  else
+    puts "not a valid entry"
+  end
+end
+
+four_digits
